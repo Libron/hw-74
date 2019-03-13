78b9aa808f311d85031def93db43d3aeb132219a
Urmat Bekboev <bek.urmat@gmail.com>
2019-03-13 07:05:45 +0600

    Added Folder Initialization Feature if does not exist

--- app/messages.js
+++ app/messages.js
@@ -2,25 +2,33 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-router.get('/', (req, res) => {
-    const path = "./app/messages";
+const dir = "./app/messages";
 
-    fs.readdir(path, (err, files) => {
+const initFolder = () => {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+};
+
+router.get('/', (req, res) => {
+    initFolder();
+    fs.readdir(dir, (err, files) => {
         if (err) throw err;
         const messages = [];
 
         files.forEach(file => {
-            const filePath = path + '/' + file;
+            const filePath = dir + '/' + file;
             const data = fs.readFileSync(filePath);
             messages.push(JSON.parse(data));
         });
-        res.send(messages.slice(Math.max(messages.length - 5, 1)));
+        res.send(messages.reverse().slice(0, 5));
     });
 });
 
 router.post('/', (req, res) => {
+    initFolder();
     const datetime = new Date().toISOString();
-    const filename = './app/messages/' + datetime + '.txt';
+    const filename = dir + '/' + datetime + '.txt';
     const reqBody = req.body;
     reqBody.dateTime = datetime;
         fs.writeFile(filename, JSON.stringify(reqBody), err => {
